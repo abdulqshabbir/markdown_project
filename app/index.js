90952139cc588bf6c6eeb3c426140a473e9da43a
Abdul Shabbir <abdulqshabbir@gmail.com>
2016-08-14 20:49:34 -0400

    Fixed Webpack Path Issue and created a Hello World Component

--- app/index.js
+++ app/index.js
@@ -1,1 +1,13 @@
-var hello = 'hello';
+var React = require('react');
+var ReactDOM = require('react-dom');
+var app = document.getElementById('app');
+
+var HelloWorld = React.createClass({
+    render: function() {
+        return (
+            <div>Hello World!</div>
+        );
+    }
+});
+
+ReactDOM.render(<HelloWorld />, app);
